fc6ef7eb8bf0aadafe8fe21758b1e09f9093bac6
Khushi Rani <151911119+KhushiRani005@users.noreply.github.com>
2025-07-03 15:04:36 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//    cout <<"Khushi rani " << "DSA First Day" <<endl;
+//    return 0;
+// }
+
+//variable
+// int main()
+// {
+//  int age= 25;
+//  char grade='a';
+//  float PI=3.14f;
+//  bool isSafe=true;
+//  double price = 100.99;
+
+//  cout<< age << endl;  
+//  cout<<PI<<endl;
+//  cout<< isSafe <<endl;
+
+//  return 0;
+
+// }
+
+// int main(){
+   // char grade='A';
+
+   // int value = grade;
+   // cout<< value << endl;
+   // return 0;
+
+   // double price = 100.99;
+   // int newPrice= (int)price;
+   // cout<< newPrice <<endl;
+   // return 0;
+// }
+
+// int main(){
+//    int age;
+//    cout << "Enter Your age:";
+//    cin >> age;
+
+//    cout << "Your Age is: " << age <<endl;
+//    return 0;
+// } 
+
+//Operators
+
+// int main(){
+//    int a=10, b=5;
+//    int sum=a+b;
+//    cout<<"sum= "<< (a+b) << endl;
+//    cout<<"difference= "<< (a-b) << endl; 
+//    cout<<"product= "<< (a*b) << endl;
+//    cout<<"division= "<< (a/b) << endl;
+//    cout<<"remainder= "<< (a%b) << endl;
+//    return 0;
+// }
+
+// int main(){
+//    cout<< (3 < 5) <<endl;
+//    cout<< (3 > 5) <<endl;
+//    cout<< (2 <= 5) <<endl;
+//    cout<< (5 >= 5) <<endl;
+//    cout<< (3 != 5) <<endl;
+// }
+
+// int main(){
+//    cout<< !(3<1) <<endl;
+//    cout<< ((3<1) || (3<5)) << endl;
+//    return 0;
+// } 
+
+//sum of two numbers
+// int main(){
+//    int a,b,sum;
+//    cout<<"enter the value of a:";
+//    cin>> a;
+//    cout<<"enter the value of b:";
+//    cin>> b;
+//    sum=a+b;
+//    cout<<"sum of two numbers:"<<sum <<endl;
+//    return 0;
+
+// }
+
+int main(){
+   // int a=10;
+   // int b=a++;
+   // cout<< "b = "<<b<< endl;
+   // cout<< "a = "<<a<< endl;
+
+   int x=10;
+   int y=x--;
+   cout<< "y = "<<y<< endl;
+   cout<< "x = "<<x<< endl;
+   return 0;
+}
