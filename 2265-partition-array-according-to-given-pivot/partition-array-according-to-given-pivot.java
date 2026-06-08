ef1501e0a9e1a7d5c57e1dadcd2c104e941bf4b0
Khushi Rani <151911119+KhushiRani005@users.noreply.github.com>
2026-06-08 18:09:20 +0530

    Time: 4 ms (99.24%) | Memory: 160 MB (49.83%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,25 +1,29 @@
 class Solution {
 
     public int[] pivotArray(int[] nums, int pivot) {
-        LinkedList<Integer> less = new LinkedList<>();
-        LinkedList<Integer> equal = new LinkedList<>();
-        LinkedList<Integer> greater = new LinkedList<>();
+        int less = 0;
+        int equal = 0;
         for (int num : nums) {
-            if (num < pivot) {
-                less.add(num);
-            } else if (num > pivot) {
-                greater.add(num);
-            } else {
-                equal.add(num);
-            }
+            if (num < pivot) less++;
+            else if (num == pivot) equal++;
         }
-        less.addAll(equal);
-        less.addAll(greater);
 
-        int i = 0;
         int[] ans = new int[nums.length];
-        for (int num : less) {
-            ans[i++] = num;
+        int lessI = 0;
+        int equalI = less;
+        int greaterI = less + equal;
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (num < pivot) {
+                ans[lessI] = num;
+                lessI++;
+            } else if (num > pivot) {
+                ans[greaterI] = num;
+                greaterI++;
+            } else {
+                ans[equalI] = num;
+                equalI++;
+            }
         }
         return ans;
     }
