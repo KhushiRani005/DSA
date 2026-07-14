e1129dbde49362b1dfb35a0476d0ce39a4cdbe36
Khushi Rani <151911119+KhushiRani005@users.noreply.github.com>
2026-07-14 16:38:57 +0530

    Time: 2 ms (99.64%) | Memory: 42.7 MB (99.28%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public String makeLargestSpecial(String s) {
+        if (s.length() <= 2) return s;
+
+        List<String> list = new ArrayList<>();
+        int count = 0;
+        int start = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1')
+                count++;
+            else
+                count--;
+
+            // Found one special substring
+            if (count == 0) {
+                String inner = makeLargestSpecial(s.substring(start + 1, i));
+                list.add("1" + inner + "0");
+                start = i + 1;
+            }
+        }
+
+        // Sort in descending order
+        Collections.sort(list, Collections.reverseOrder());
+
+        StringBuilder ans = new StringBuilder();
+        for (String str : list) {
+            ans.append(str);
+        }
+
+        return ans.toString();
+    }
+}
